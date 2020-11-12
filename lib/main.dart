d481d49e471e555d266ddf120c96cb3df87513f9
sysvic <victorhpantunes@gmail.com>
2020-11-12 20:07:50 -0300

    adding build.gradle correct app name

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gbdmobile/bloc/auth.bloc.dart';
 
 void main() {
   runApp(MyApp());
@@ -24,16 +25,52 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  Auth _auth;
 
+  @override
+  void initState() {
+    _auth = Auth();
+    super.initState();
+  }
 
-  
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         constraints: BoxConstraints.expand(),
         child: Column(
-          children: [Expanded(), Expanded(), Expanded()],
+          children: [
+            Expanded(
+              child: Container(),
+            ),
+            Expanded(
+                child: Row(
+              children: [
+                Expanded(
+                  flex: 3,
+                  child: FlatButton(
+                    onPressed: () => _auth.signInWithGihub(),
+                    child: Container(
+                      
+                      child: Text('Login with Github'),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Image.network(
+                      'https://img.icons8.com/fluent/48/000000/github.png'),
+                )
+              ],
+            )),
+            Expanded(
+              child: Container(),
+            )
+          ],
         ),
       ),
     );
