d36133dccf5a92930b5f71564597e2d232407ff3
sysvic <victorhpantunes@gmail.com>
2020-11-19 10:39:38 -0300

    feat#01/creating funtion to create user in firebase

--- lib/bloc/auth.bloc.dart
+++ lib/bloc/auth.bloc.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart' as auth;
 import 'package:flutter/widgets.dart';
 import 'package:gbdmobile/secret_keys.dart' as SecretKey;
 import 'package:github_auth/github_auth.dart';
@@ -5,19 +6,36 @@ import 'package:github_auth/github_auth.dart';
 class AuthService {
   static bool githubAuth({@required BuildContext context}) {
     try {
-      final auth = GithubAuth(
+      final githubAuth = GithubAuth(
         clientId: SecretKey.CLIENT_ID,
         clientSecret: SecretKey.CLIENT_SECRET,
-        callbackUrl: 'https://git-breakdown-mobile.firebaseapp.com/__/auth/handler',
+        callbackUrl:
+            'https://git-breakdown-mobile.firebaseapp.com/__/auth/handler',
       );
 
-      
-      auth.login(context).then((value) {
+      githubAuth.login(context).then((value) {
         print('(SYS) token' + value.token.toString());
+        createFirebaseUser(token: value.token);
       });
       return true;
     } catch (error) {
       print('(SYS) error' + error.toString());
+      return false;
+    }
+  }
+
+  static Future<bool> createFirebaseUser({@required token}) async {
+    try {
+      final auth.AuthCredential credential =
+          auth.GithubAuthProvider.credential(token);
+
+      await auth.FirebaseAuth.instance.signInWithCredential(credential);
+
+      return true;
+    } catch (error) {
+      print(
+        '(SYS) error : ' + error.toString(),
+      );
       return false;
     }
   }
