fb1e826f8e55b968e3a907d49899a2c92578a5ad
wdvictor <victorhpantunes@gmail.com>
2020-12-01 12:24:29 -0300

    feat#4/adding bloc to commits request

---
+++ lib/bloc/commitsRequest.bloc.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+import 'auth.bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:gbdmobile/bloc/LoggedUser.dart';
+import 'package:http/http.dart' as http;
+
+class CommitsRequest {
+  static Future<Map<String, int>> getCommits(
+      {@required String repository, @required String owner}) async {
+    String userToken = await AuthService.readData();
+    userToken = userToken.replaceAll(RegExp('"'), '');
+
+    ///This map return the github username as a Key and the total
+    ///of commits as value
+    Map<String, int> userCommitsMap = {};
+    final String githubApi =
+        "https://git-breakdown-mobile.web.app/commits?owner=$owner&repository=$repository&token=$userToken";
+
+    http.Client client = http.Client();
+    var response = await client.get(githubApi);
+    final parsed = json.decode(response.body);
+
+    try {
+      for(var x in parsed){
+        if(x is List) break;
+
+       
+          String username = x['name'];
+          int userCommits = x['commits'];
+          userCommitsMap[username] = userCommits;
+       
+      }
+    } catch (err) {
+      return null;
+    }
+
+    return userCommitsMap;
+  }
+}
