dbcfae1ea1e899515b298aa62da4cbf866cfa5b7
jbisinotti <victormax2006@gmail.com>
2020-12-02 02:35:33 -0300

    feat(#3):last adjustments on issues request

--- lib/bloc/issuesRequest.bloc.dart
+++ lib/bloc/issuesRequest.bloc.dart
@@ -4,13 +4,13 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class IssuesRequest {
-  static Future<Map<String, int>> getIssues(
+  static Future<Map<String, num>> getIssues(
       {@required String repository, @required String owner}) async {
     String userToken = await AuthService.readData();
     userToken = userToken.replaceAll(RegExp('"'), '');
 
 
-    Map<String, int> issuesMap = {};
+    Map<String, num> issuesMap = {};
     final String githubApi =
         "https://git-breakdown-mobile.web.app/issues?owner=$owner&repository=$repository&token=$userToken";
 
