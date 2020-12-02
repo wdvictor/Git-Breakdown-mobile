17242f0f0cec90636ed1651cfae75b2270ed7030
pdaniel37 <pedrodaniel@candev.com.br>
2020-12-02 17:18:27 -0300

    feat(#12): Creating HomePage

---
+++ lib/ui/homePage.dart
@@ -0,0 +1,53 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:gbdmobile/Models/user.dart';
+import 'package:gbdmobile/bloc/LoggedUser.dart';
+import 'package:gbdmobile/bloc/auth.bloc.dart';
+import 'package:gbdmobile/bloc/reposRequest.bloc.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  GbdUser _user = GbdUser(
+      userName: "username",
+      clientToken: "token",
+      photoUrl: "",
+      email: "Email do Usuário",
+      displayName: "Nome do Usuário");
+  List<String> _userRepos = [];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Git BreakDown"),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            UserAccountsDrawerHeader(
+                accountName: Text(_user.displayName),
+                accountEmail: Text(_user.email),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(_user.photoUrl),
+                ),
+            ),
+          ],
+        ),
+      ),
+      body: Container(),
+    );
+  }
+
+}
+
