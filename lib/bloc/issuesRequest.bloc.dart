9506e913ac58aabffcaa329ed7cb25bb300e5283
jbisinotti <victormax2006@gmail.com>
2020-12-02 01:48:24 -0300

    feat(#3): creating a bloc to put issues metrics into a map and deleting previous model

---
+++ lib/bloc/issuesRequest.bloc.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+import 'auth.bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class IssuesRequest {
+  static Future<Map<String, int>> getIssues(
+      {@required String repository, @required String owner}) async {
+    String userToken = await AuthService.readData();
+    userToken = userToken.replaceAll(RegExp('"'), '');
+
+
+    Map<String, int> issuesMap = {};
+    final String githubApi =
+        "https://git-breakdown-mobile.web.app/issues?owner=$owner&repository=$repository&token=$userToken";
+
+    http.Client client = http.Client();
+    var response = await client.get(githubApi);
+    final parsed = json.decode(response.body);
+
+    try {
+      issuesMap["open"] = parsed["open"];
+      issuesMap["closed"] = parsed["closed"];
+      issuesMap["openPercent"] = parsed["openPercent"];
+      issuesMap["closedPercent"] = parsed["closedPercent"];
+    } catch (err) {
+      return null;
+    }
+
+    return issuesMap;
+  }
+}
